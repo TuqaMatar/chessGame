f61423e85cd47d4614b24e239215c992291f0e30
tuqa <tuqabumatar@gmail.com>
2022-12-27 23:22:59 +0300

    add Knight legal movement

--- src/com/company/Pieces/Knight.java
+++ src/com/company/Pieces/Knight.java
@@ -10,6 +10,17 @@ public class Knight extends Piece{
 
     @Override
     public boolean isMoveLegal(Position currentPosition , Position nextPosition) {
+        //knight has 8 possible movements i will check all of them
+        if(nextPosition.getRank()== currentPosition.getRank()+2 && nextPosition.getFile() == currentPosition.getFile()+1
+         ||nextPosition.getRank()== currentPosition.getRank()+2 && nextPosition.getFile() == currentPosition.getFile()-1
+         ||nextPosition.getRank()== currentPosition.getRank()-2 && nextPosition.getFile() == currentPosition.getFile()+1
+         ||nextPosition.getRank()== currentPosition.getRank()-2 && nextPosition.getFile() == currentPosition.getFile()-1
+
+         ||nextPosition.getRank()== currentPosition.getRank()+1 && nextPosition.getFile() == currentPosition.getFile()+2
+         ||nextPosition.getRank()== currentPosition.getRank()-1 && nextPosition.getFile() == currentPosition.getFile()+2
+         ||nextPosition.getRank()== currentPosition.getRank()+1 && nextPosition.getFile() == currentPosition.getFile()-2
+         ||nextPosition.getRank()== currentPosition.getRank()-1 && nextPosition.getFile() == currentPosition.getFile()-2)
+            return true;
         return false;
     }
     @Override
