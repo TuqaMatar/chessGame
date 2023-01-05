79d1b177cc908c4f4b62ade0957a06bc62541f02
tuqa <tuqabumatar@gmail.com>
2023-01-05 22:33:13 +0300

    cleaned castle legal moves

--- src/com/company/Pieces/Castle.java
+++ src/com/company/Pieces/Castle.java
@@ -4,6 +4,8 @@ import com.company.ChessBoard;
 import com.company.PieceColor;
 import com.company.Position;
 
+import java.util.ArrayList;
+
 public class Castle extends Piece{
     public Castle(PieceColor pieceColor) {
         super(pieceColor);
@@ -13,73 +15,40 @@ public class Castle extends Piece{
     @Override
     public void updateLegalMoves(Position currentPosition,ChessBoard chessBoard) {
         legalMoves.clear();
-        //add pieces that are attacked vertically
-        char file = currentPosition.getFile();
-        char rank = currentPosition.getRank();
 
-        for (int i = Character.getNumericValue(currentPosition.getRank()-1); i >= 1; i--) {
-            if (chessBoard.getPositionAt(file, (char)(i+'0')) != null) {
+        ArrayList<Position> startingPositions = new ArrayList<>();
 
-                if (chessBoard.getPositionAt(file, (char)(i+'0')).isEmpty())
-                    legalMoves.add(chessBoard.getPositionAt(file, (char)(i+'0')));
-                else {
-                    if (chessBoard.getPositionAt(file, (char)(i+'0')).getPiece().pieceColor != pieceColor) {
-                        legalMoves.add(chessBoard.getPositionAt(file, (char) (i)));
+        int[][] offsets = {{0, +1}, {0, -1}, {+1, 0}, {-1, 0}};
+        startingPositions.add(chessBoard.getPositionAt((char) (currentPosition.getFile() + offsets[0][0]), currentPosition.getRank() + offsets[0][1])); // right diagonal forward
+        startingPositions.add(chessBoard.getPositionAt((char) (currentPosition.getFile() + offsets[1][0]), currentPosition.getRank() +offsets[1][1])); //right diagonal backward
+        startingPositions.add(chessBoard.getPositionAt((char) (currentPosition.getFile() + offsets[2][0]), currentPosition.getRank() + offsets[2][1])); //left diagonal forward
+        startingPositions.add(chessBoard.getPositionAt((char) (currentPosition.getFile() + offsets[3][0]), currentPosition.getRank() +offsets[3][1]));//left diagonal backward
+
+
+        for (int i = 0; i < startingPositions.size(); i++) {
+            Position testPosition = startingPositions.get(i);
+            if (testPosition != null) {
+                while (testPosition != null) {
+                    Position positionToCheck = chessBoard.getPositionAt((chessBoard.getPositionAt(testPosition.getFile(), testPosition.getRank())));
+                    if (positionToCheck != null) {
+                        if (positionToCheck.isEmpty())
+                            legalMoves.add(positionToCheck);
+                        else {
+                            if (positionToCheck.getPiece().pieceColor != pieceColor) {
+                                legalMoves.add(positionToCheck);
+                            }
+                            break;
+                        }
+
                     }
-                    break;
+
+                    testPosition = chessBoard.getPositionAt((char) (testPosition.getFile() + offsets[i][0]), testPosition.getRank() + offsets[i][1]);
+
                 }
             }
-
         }
 
-        for ( int i = Character.getNumericValue(currentPosition.getRank()+1); i <= 8; i++) {
-            if (chessBoard.getPositionAt(file, (char)(i+'0')) != null) {
-                if (chessBoard.getPositionAt(file, (char)(i+'0')).isEmpty())
-                    legalMoves.add(chessBoard.getPositionAt(file, (char)(i+'0')));
-                else {
-                    if (chessBoard.getPositionAt(file, (char)(i+'0')).getPiece().pieceColor != pieceColor) {
-                        legalMoves.add(chessBoard.getPositionAt(file, (char)(i+'0')));
-                    }
-                    break;
-                }
-            }
-
-        }
-
-
-        //add pieces that are attacked horizontally
-        for (char i = (char) (currentPosition.getFile() -1); i >= 'a'; i--) {
-            if (chessBoard.getPositionAt(i, rank) != null) {
-
-                if (chessBoard.getPositionAt(i, rank).isEmpty())
-                    legalMoves.add(chessBoard.getPositionAt(i, rank));
-                else {
-                    if (chessBoard.getPositionAt(i, rank).getPiece().pieceColor != pieceColor) {
-                        legalMoves.add(chessBoard.getPositionAt(i, rank));
-                    }
-                    break;
-                }
-            }
-
-
-        }
-
-        for (char i = (char) (currentPosition.getFile() + 1); i <= 'h'; i++) {
-            if (chessBoard.getPositionAt(i, rank) != null) {
-
-                if (chessBoard.getPositionAt(i, rank).isEmpty())
-                    legalMoves.add(chessBoard.getPositionAt(i, rank));
-                else {
-                    if (chessBoard.getPositionAt(i, rank).getPiece().pieceColor != pieceColor) {
-                        legalMoves.add(chessBoard.getPositionAt(i, rank));
-                    }
-                    break;
-                }
-
-
-            }
-
-        }    }
+    }
 
     @Override
     public String toString() {
